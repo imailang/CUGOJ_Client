0ab06f8909f99a4c60dd12f94ceaa933925fa24b
zzj <80336416+zzj202@users.noreply.github.com>
2022-05-12 01:32:00 +0800

    Update OjHome.vue

--- src/views/oj/OjHome.vue
+++ src/views/oj/OjHome.vue
@@ -5,18 +5,18 @@
         <template #header>
           <b style="font-size: 30px;color: #308ff1">欢迎来到CUGOJ</b>
         </template>
-        <el-carousel :interval="1000" arrow="always" height="600px">
+        <el-carousel :interval="1000" arrow="always" height="400px">
           <el-carousel-item>
             <el-image
-                src="https://img14.360buyimg.com/pop/jfs/t1/174086/38/25421/123679/61cfd23eEad2850f2/245b204eb519851b.jpg"></el-image>
+                src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1114%2F0G020114924%2F200G0114924-15-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654881502&t=574f7868da85c7a0e6ecbbf16d299063"></el-image>
           </el-carousel-item>
           <el-carousel-item>
             <el-image
-                src="https://img14.360buyimg.com/pop/jfs/t1/120947/29/18644/69926/616651caE22e7719a/ada55dd670aec9c8.jpg"></el-image>
+                src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1114%2F113020142315%2F201130142315-1-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654881502&t=426f9764f353d3393f8bc49baf316eda"></el-image>
           </el-carousel-item>
           <el-carousel-item>
             <el-image
-                src="https://img14.360buyimg.com/pop/jfs/t1/142451/23/25535/182699/61cfd245Ef54789d4/f798f1cd6f6c5b01.jpg"></el-image>
+                src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1113%2F052420110515%2F200524110515-11-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654881502&t=eb260aa4218df595d4693a77f96a9e6e"></el-image>
           </el-carousel-item>
         </el-carousel>
       </el-card>
